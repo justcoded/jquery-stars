77a92e57b8149ece46ed00ad713f01243632fa7e
Stanislav Gurin <sgurin@justcoded.com>
2016-02-11 17:11:12 +0200

    Adding Sections

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -1,34 +1,53 @@
 $(document).ready(function(){
 	hljs.initHighlightingOnLoad();
-	$('#stars1').jstars({
+	initAnchors();
+	initStars();
+})
+// initialize smooth anchor links
+function initAnchors() {
+	new SmoothScroll({
+		anchorLinks: '.navbar a[href^="#"]:not([href="#"])',
+		extraOffset: function() {
+			var totalHeight = 0;
+			jQuery('.navbar').each(function(){
+				totalHeight += jQuery(this).outerHeight();
+			});
+			return totalHeight;
+		},
+		activeClasses: 'link'
+	});
+}
+
+// initialize stars
+function initStars() {
+	$('#example-1').jstars({
 		image_path: 'images'
 	});
 
-	$('#stars2').jstars({
+	$('#example-2').jstars({
 		image_path: 'images',
 		style: 'blue'
 	});
 
-	$('#stars3').jstars({
+	$('#example-3').jstars({
 		image_path: 'images',
 		style: 'yellow',
 		frequency: 19
 	});
 
-	$('#stars4').jstars({
+	$('#example-4').jstars({
 		image_path: 'images',
 		style: 'green'
 	});
 
-	$('#stars5').jstars({
+	$('#example-5').jstars({
 		image_path: 'images',
 		style: 'red',
 		frequency: 1
 	});
 
-	$('#stars6').jstars({
+	$('#example-6').jstars({
 		image_path: 'images',
 		style: 'rand'
 	});
-	
-})
+}
